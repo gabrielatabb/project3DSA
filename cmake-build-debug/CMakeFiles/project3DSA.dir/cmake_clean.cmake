559b2be833c33f993048e354e64323d5a7f58b08
dsantosgonzalez <santosdiegosd@gmail.com>
2023-12-03 18:26:12 -0500

    heyyyyy

---
+++ cmake-build-debug/CMakeFiles/project3DSA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project3DSA.dir/main.cpp.o"
+  "CMakeFiles/project3DSA.dir/main.cpp.o.d"
+  "project3DSA"
+  "project3DSA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project3DSA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
